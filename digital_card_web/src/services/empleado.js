a2864b0e646d0fee7104b2e3c14d4e485f8ab84c
pieroleonyaipen07@gmail.com <pieroleonyaipen07@gmail.com>
2023-11-30 23:39:31 -0500

    Crud Empleado Terminado

--- digital_card_web/src/services/empleado.js
+++ digital_card_web/src/services/empleado.js
@@ -9,7 +9,14 @@ var form = document.getElementById("myForm"),
     sDate = document.getElementById("sDate"),
     submitBtn = document.querySelector(".submit"),
     userInfo = document.getElementById("data"),
-    tablaBody = document.getElementById("databody")
+    tablaBody = document.getElementById("databody"),
+    showName = document.getElementById("showName"),
+    showLastname = document.getElementById("showLastname"),
+    showMothername = document.getElementById("showMothername"),
+    showDni = document.getElementById("showDni"),
+    showTipo = document.getElementById("showTipo"),
+    showDate = document.getElementById("showDate"),
+    btnEditarEmpleado = document.getElementById("btnEditarEmpleado")
 
 async function loadEmployees() {
     const response = await axios.get("https://n24kmjvt-3000.brs.devtunnels.ms/api/employees")
@@ -18,7 +25,6 @@ async function loadEmployees() {
     for (let employee of reponsedata) {
         tablaBody.innerHTML += createComponentDataBody(employee["EmployeeID"], employee["Dni"], employee["Names"], employee["Lastname"], employee["MotherLastname"], employee["DocumentType"], employee["CreationDate"], employee["IsActive"])
     }
-    console.log(response);
 }
 
 loadEmployees()
@@ -33,57 +39,57 @@ function createComponentDataBody(EmployeeID, Dni, Names, Lastname, MotherLastnam
         <td>${IsActive}</td>
         <td>${CreationDate}</td>
         <td>
-            <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#readData">
-             <i class="bi bi-eye"></i></button>
-            <button class="btn btn-primary" onclick = "editInfo('${Names}','${Lastname}','${MotherLastname}','${Dni}','${DocumentType}','${CreationDate}')"><i class="bi bi-pencil"></i></button>
+            <button class="btn btn-success" onclick = "readInfo('${Names}','${Lastname}','${MotherLastname}','${Dni}','${DocumentType}','${CreationDate}')" data-bs-toggle="modal" data-bs-target="#readData">
+             <i class="bi bi-pencil"></i></button>
             <button class="btn btn-danger" onclick = "deleteInfo(${EmployeeID})"><i class="bi bi-trash"></i></button>
          </td>
           </tr>
     `
 }
 
-
-function readInfo(pic, name, dni, tip, sDate) {
-    document.querySelector('.showImg').src = pic,
-        document.querySelector('#showName').value = name,
-        document.querySelector('#showDni').value = dni,
-        document.querySelector('#ShowTwipo').value = tip,
-        document.querySelector('#showsDate').value = sDate
-}
-
-async function editInfo(name,lastname,mothername,dni,tipo,creationdate) {
-    isEdit = true
- 
-
-    submitBtn.innerText = "Update"
+function readInfo(name, lastname, mothername, dni, tipo, creationdate) {
+    showName.value = name
+    showLastname.value = lastname
+    showMothername.value = mothername
+    showDni.value = dni
+    showTipo.value = tipo
+    showDate.value = creationdate
 
 }
+
+btnEditarEmpleado.addEventListener("click", async(e) => {
+    const information = {
+        "Dni": showDni.value,
+        "Names": showName.value,
+        "Lastname": showLastname.value,
+        "MotherLastname": showMothername.value,
+        "DocumentType": showTipo.value
+    }
+    await axios.put(`https://n24kmjvt-3000.brs.devtunnels.ms/api/employees`, information)
+    loadEmployees()
+})
 
 async function deleteInfo(EmployeeID) {
     if (confirm("Estas seguro que quieres eliminar?")) {
-    await axios.delete(`https://n24kmjvt-3000.brs.devtunnels.ms/api/employees/${EmployeeID}`)  
+        await axios.delete(`https://n24kmjvt-3000.brs.devtunnels.ms/api/employees/${EmployeeID}`)
         loadEmployees()
     }
 }
 
 form.addEventListener('submit', async (e) => {
     e.preventDefault()
-
     const information = {
-        // employeeName: Username.value,
-        // employeeDni: dni.value,
-        // employeeTipo: tipo.value,
-            "Dni": dni.value,
-            "Names": Username.value,
-            "Lastname": Lastname.value,
-            "MotherLastname": Mothername.value,
-            "DocumentType": tipo.value
+        "Dni": dni.value,
+        "Names": Username.value,
+        "Lastname": Lastname.value,
+        "MotherLastname": Mothername.value,
+        "DocumentType": tipo.value
     }
 
     submitBtn.innerText = "Submit"
 
     form.reset()
 
-    await axios.post("https://n24kmjvt-3000.brs.devtunnels.ms/api/employees",information)
+    await axios.post("https://n24kmjvt-3000.brs.devtunnels.ms/api/employees", information)
     loadEmployees()
 })
