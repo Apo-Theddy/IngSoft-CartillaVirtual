301a611943140f70ce56bd9e5c070925c7071bd6
pieroleonyaipen07@gmail.com <pieroleonyaipen07@gmail.com>
2023-11-30 21:54:28 -0500

    Crud Empleados

--- digital_card_web/src/services/empleado.js
+++ digital_card_web/src/services/empleado.js
@@ -2,118 +2,88 @@ var form = document.getElementById("myForm"),
     imgInput = document.querySelector(".img"),
     file = document.getElementById("imgInput"),
     Username = document.getElementById("name"),
+    Lastname = document.getElementById("Lastname"),
+    Mothername = document.getElementById("Mothername"),
     dni = document.getElementById("dni"),
     tipo = document.getElementById("tipo"),
     sDate = document.getElementById("sDate"),
     submitBtn = document.querySelector(".submit"),
-    userInfo = document.getElementById("data")
+    userInfo = document.getElementById("data"),
+    tablaBody = document.getElementById("databody")
 
-let getData = localStorage.getItem('userProfile') ? JSON.parse(localStorage.getItem("userProfile")) : []
-
-let isEdit = false, editId
-showInfo()
-
-file.onchange = function () {
-    if (file.files[0].size < 1000000) {
-        var fileReader = new FileReader();
-
-        fileReader.onload = function (e) {
-            imgUrl = e.target.result
-            imgInput.src = imgUrl
-        }
-        fileReader.readAsDataURL(file.files[0])
-    } else {
-        alert("El archivo es demasiado grande!")
+async function loadEmployees() {
+    const response = await axios.get("https://n24kmjvt-3000.brs.devtunnels.ms/api/employees")
+    let reponsedata = response.data
+    tablaBody.innerHTML = ''
+    for (let employee of reponsedata) {
+        tablaBody.innerHTML += createComponentDataBody(employee["EmployeeID"], employee["Dni"], employee["Names"], employee["Lastname"], employee["MotherLastname"], employee["DocumentType"], employee["CreationDate"], employee["IsActive"])
     }
+    console.log(response);
 }
 
-function showInfo() {
-    document.querySelectorAll('.employeeDetails').forEach(info => info.remove())
-    getData.forEach((element, index) => {
-        let createElement = `<tr class="employeeDetails">
-            <td>${index + 1}</td>
-            <td><img src="${element.picture}" alt="" width="50" height="50"</td>
-            <td>${element.employeeName}</td>
-            <td>${element.employeeDni}</td>
-            <td>${element.employeeTipo}</td>
-            <td>${element.startDate}</td>
+loadEmployees()
 
-            <td>
-                <button class="btn btn-success" onclick="readInfo('${element.picture}','${element.employeeName}',
-                    '${element.employeeDni}','${element.employeeTipo}','${element.startDate}') 
-                    data-bs-toggle="modal" data-bs-target="#readData"><i class="bi bi-eye"></i></button>
-
-                <button class="btn btn-primary"onclick="editInfo(${index},'${element.picture}','${element.employeeName}',
-                '${element.employeeDni}','${element.employeeTipo}','${element.startDate}') "data-bs-toggle="modal" data-bs-target="#userForm">
-                <i class="bi bi-pencil"></i></button>
-
-                <button class="btn btn-danger" onclick ="deleteInfo(${index})"><i class="bi bi-trash"></i></button>
-            </td>
-
-        </tr>`
-
-        userInfo.innerHTML += createElement
-    })
+function createComponentDataBody(EmployeeID, Dni, Names, Lastname, MotherLastname, DocumentType, CreationDate, IsActive) {
+    return `
+    <tr>
+        <td>${EmployeeID}</td>
+        <td>${Names} ${Lastname} ${MotherLastname}</td>
+        <td>${Dni}</td>
+        <td>${DocumentType}</td>
+        <td>${IsActive}</td>
+        <td>${CreationDate}</td>
+        <td>
+            <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#readData">
+             <i class="bi bi-eye"></i></button>
+            <button class="btn btn-primary" onclick = "editInfo('${Names}','${Lastname}','${MotherLastname}','${Dni}','${DocumentType}','${CreationDate}')"><i class="bi bi-pencil"></i></button>
+            <button class="btn btn-danger" onclick = "deleteInfo(${EmployeeID})"><i class="bi bi-trash"></i></button>
+         </td>
+          </tr>
+    `
 }
-showInfo()
 
-function readInfo(pic,name,dni,tip,sDate){
+
+function readInfo(pic, name, dni, tip, sDate) {
     document.querySelector('.showImg').src = pic,
-    document.querySelector('#showName').value = name,
-    document.querySelector('#showDni').value = dni,
-    document.querySelector('#ShowTipo').value = tip,
-    document.querySelector('#showsDate').value = sDate
+        document.querySelector('#showName').value = name,
+        document.querySelector('#showDni').value = dni,
+        document.querySelector('#ShowTwipo').value = tip,
+        document.querySelector('#showsDate').value = sDate
 }
 
-function editInfo(pic,name,dni,tip,sDate){
+async function editInfo(name,lastname,mothername,dni,tipo,creationdate) {
     isEdit = true
-    editId = index
-    imgInput.src = pic
-    Username.value=name
-    dni.value = dni
-    tipo.value = tip
-    sDate.value = sDate
+ 
 
     submitBtn.innerText = "Update"
-    modalTitle.innerText = "Cambiar datos"
+
 }
 
-function deleteInfo(index){
-    if(confirm("Estas seguro que quieres eliminar?")){
-        getData.splice(index,1)
-        localStorage.setItem("userProfile", JSON.stringify(getData))
-        showInfo()
+async function deleteInfo(EmployeeID) {
+    if (confirm("Estas seguro que quieres eliminar?")) {
+    await axios.delete(`https://n24kmjvt-3000.brs.devtunnels.ms/api/employees/${EmployeeID}`)  
+        loadEmployees()
     }
 }
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', async (e) => {
     e.preventDefault()
 
     const information = {
-        picture: imgInput.src == undefined ? "../../imgs/logo.png" : imgInput.src,
-        employeeName: Username.value,
-        employeeDni: dni.value,
-        employeeTipo: tipo.value,
-        startDate: sDate.value
+        // employeeName: Username.value,
+        // employeeDni: dni.value,
+        // employeeTipo: tipo.value,
+            "Dni": dni.value,
+            "Names": Username.value,
+            "Lastname": Lastname.value,
+            "MotherLastname": Mothername.value,
+            "DocumentType": tipo.value
     }
-
-    if (!isEdit) {
-        getData.push(information)
-    } else {
-        isEdit = false
-        getData[editId] = information
-    }
-
-    localStorage.setItem('userProfile', JSON.stringify(getData))
 
     submitBtn.innerText = "Submit"
-    modalTitle.innerHtml = "Fill The Form"
-
-    showInfo()
 
     form.reset()
 
-    imgInput.src = "../../imgs/logo.png"
-    modal.style.display = "none"
-    document.querySelector(".modal-backdrop").remove()
+    await axios.post("https://n24kmjvt-3000.brs.devtunnels.ms/api/employees",information)
+    loadEmployees()
 })
--- digital_card_web/src/services/index.js
+++ digital_card_web/src/services/index.js
@@ -38,81 +38,81 @@ function closePopup() {
     popup.style.display = 'none';
 }
 
-function showEditPopUp(...data) {
-    const popup = document.getElementById('popup2');
-    popup.style.display = 'block';
-    divEditar.innerHTML = `
-     <div class="titulopop">Editar</div>
-                    <form action="">
-                        <p>
-                            <label for="NombrePlato">Platillo :</label>
-                            <input type="text" name="nombreplatillo" id="frmEditDishName" value='${data[0]}'>
-                        </p>
-                        <p>
-                            <label for="Preciop">Precio :</label>
-                            <input type="number" name="preciop" id="frmEditUnitPrice" min="0" value='${data[1]}' >
-                        </p>
-                        <p>
-                            <label for="cantidadp">Cantidad :</label>
-                            <input type="number" name="cantidadp" id="frmEditQuantityAvailable" min="0"  value='${data[2]}'>
-                        </p>
-                        <p>
-                            <label for="Categoriap">Cantegoria :</label>
-                            <input type="number" name="Categoriap" id="frmEditCategory" min="0"> 
-                        </p>
-                        <p class="block">
-                            <label for="Descripcionp">Descripción :</label>
-                            <textarea id="frmEditDescription" name="Descripcionp" rows="4"></textarea value='${data[3]}'>
-                        </p>
-                        <p class="block">
-                            <button type="editarb" id="btnEditButton">Editar</button>
-                        </p>
-                    </form>`
+// function showEditPopUp(...data) {
+//     const popup = document.getElementById('popup2');
+//     popup.style.display = 'block';
+//     divEditar.innerHTML = `
+//      <div class="titulopop">Editar</div>
+//                     <form action="">
+//                         <p>
+//                             <label for="NombrePlato">Platillo :</label>
+//                             <input type="text" name="nombreplatillo" id="frmEditDishName" value='${data[0]}'>
+//                         </p>
+//                         <p>
+//                             <label for="Preciop">Precio :</label>
+//                             <input type="number" name="preciop" id="frmEditUnitPrice" min="0" value='${data[1]}' >
+//                         </p>
+//                         <p>
+//                             <label for="cantidadp">Cantidad :</label>
+//                             <input type="number" name="cantidadp" id="frmEditQuantityAvailable" min="0"  value='${data[2]}'>
+//                         </p>
+//                         <p>
+//                             <label for="Categoriap">Cantegoria :</label>
+//                             <input type="number" name="Categoriap" id="frmEditCategory" min="0"> 
+//                         </p>
+//                         <p class="block">
+//                             <label for="Descripcionp">Descripción :</label>
+//                             <textarea id="frmEditDescription" name="Descripcionp" rows="4"></textarea value='${data[3]}'>
+//                         </p>
+//                         <p class="block">
+//                             <button type="editarb" id="btnEditButton">Editar</button>
+//                         </p>
+//                     </form>`
 
-    const btnEdit = document.getElementById("btnEditButton");
-    btnEdit.addEventListener("click", (event) => {
-        event.preventDefault();
-        const DishName = document.getElementById("frmEditDishName").value
-        const UnitPrice = parseFloat(document.getElementById("frmEditUnitPrice").value)
-        const QuantityAvailable = parseInt(document.getElementById("frmEditQuantityAvailable").value)
-        const Description = document.getElementById("frmEditDescription").value.trim();
-        data = { DishName, UnitPrice, QuantityAvailable, Description };
-        console.log(data);
-    })
-}
-
-
-function closePopup2() {
-    const popup = document.getElementById('popup2');
-    popup.style.display = 'none';
-}
+//     const btnEdit = document.getElementById("btnEditButton");
+//     btnEdit.addEventListener("click", (event) => {
+//         event.preventDefault();
+//         const DishName = document.getElementById("frmEditDishName").value
+//         const UnitPrice = parseFloat(document.getElementById("frmEditUnitPrice").value)
+//         const QuantityAvailable = parseInt(document.getElementById("frmEditQuantityAvailable").value)
+//         const Description = document.getElementById("frmEditDescription").value.trim();
+//         data = { DishName, UnitPrice, QuantityAvailable, Description };
+//         console.log(data);
+//     })
+// }
 
 
-getDishes().then((dishes) => {
-    for (let i = 0; i < dishes.length; ++i) dishContent.innerHTML += createDishComponent(dishes[i])
-});
+// function closePopup2() {
+//     const popup = document.getElementById('popup2');
+//     popup.style.display = 'none';
+// }
 
-btnAddDish.addEventListener("click", async (event) => {
-    event.preventDefault();
-    let DishName = document.getElementById("frmDishName").value
-    let UnitPrice = parseFloat(document.getElementById("frmUnitPrice").value)
-    let QuantityAvailable = parseInt(document.getElementById("frmQuantityAvailable").value)
-    let Description = document.getElementById("frmDescription").value.trim();
-    let data = { DishName, UnitPrice, QuantityAvailable, Description: Description === "" ? null : Description }
-    let dish = await addDish(data);
-    dishContent.innerHTML += createDishComponent(dish);
-})
 
-function createDishComponent(dish) {
-    let verifyImage = dish["Images"] !== undefined && dish["Images"].length > 0 ? `http://localhost:3000/${dish.images[0].Path}` : "https://img.freepik.com/vector-premium/image-icon-vector-illustration-photo-on-isolated-background-gallery-sign-concept_993513-11.jpg";
-    return `<div class="general-txt">
-                <img src="${verifyImage}" alt="">
-                    <h3>${dish["DishName"]}</h3>
-                    <p>${dish["Description"] ?? "Sin Descripcion"}</p>
-                    <div class="prices">
-                        <span>S/ ${dish["UnitPrice"]}</span>
-                        <button class="btn-2" onclick="showEditPopUp('${dish["DishName"]}',${dish["UnitPrice"]},${dish["QuantityAvailable"]},${dish["Description"] === null ? "" : dish["Description"]})">Editar</button>
-                    </div>
-            </div>`;
-}
+// getDishes().then((dishes) => {
+//     for (let i = 0; i < dishes.length; ++i) dishContent.innerHTML += createDishComponent(dishes[i])
+// });
+
+// btnAddDish.addEventListener("click", async (event) => {
+//     event.preventDefault();
+//     let DishName = document.getElementById("frmDishName").value
+//     let UnitPrice = parseFloat(document.getElementById("frmUnitPrice").value)
+//     let QuantityAvailable = parseInt(document.getElementById("frmQuantityAvailable").value)
+//     let Description = document.getElementById("frmDescription").value.trim();
+//     let data = { DishName, UnitPrice, QuantityAvailable, Description: Description === "" ? null : Description }
+//     let dish = await addDish(data);
+//     dishContent.innerHTML += createDishComponent(dish);
+// })
+
+// function createDishComponent(dish) {
+//     let verifyImage = dish["Images"] !== undefined && dish["Images"].length > 0 ? `http://localhost:3000/${dish.images[0].Path}` : "https://img.freepik.com/vector-premium/image-icon-vector-illustration-photo-on-isolated-background-gallery-sign-concept_993513-11.jpg";
+//     return `<div class="general-txt">
+//                 <img src="${verifyImage}" alt="">
+//                     <h3>${dish["DishName"]}</h3>
+//                     <p>${dish["Description"] ?? "Sin Descripcion"}</p>
+//                     <div class="prices">
+//                         <span>S/ ${dish["UnitPrice"]}</span>
+//                         <button class="btn-2" onclick="showEditPopUp('${dish["DishName"]}',${dish["UnitPrice"]},${dish["QuantityAvailable"]},${dish["Description"] === null ? "" : dish["Description"]})">Editar</button>
+//                     </div>
+//             </div>`;
+// }
 
